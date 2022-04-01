4b516553f958887d1dc30ec6605f6e770582b132
Jessica Arruda <sicarruda@gmail.com>
2022-04-01 17:22:56 -0300

    exercicio 13

--- 13_logica_01.py
+++ 13_logica_01.py
@@ -6,3 +6,15 @@ você faça um programa que leia a variável peso (peso de peixes) e calcule o
 excesso. Gravar na variável excesso a quantidade de quilos além do limite e na
 variável multa o valor da multa que João deverá pagar. Imprima os dados do
 programa com as mensagens adequadas. '''
+
+
+def calculadora_exesso_peso(peso):
+    multa = 4
+    peso_maximo = 50
+    valor_multa = 0
+    exesso = peso - peso_maximo
+    if exesso <= 0 :
+        return 0
+    valor_multa = exesso * multa 
+    return valor_multa
+      
