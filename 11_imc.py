4d57f1d168e35ae4f40e34ef5fb2046a681e5c63
Jessica Arruda <sicarruda@gmail.com>
2022-04-01 15:58:41 -0300

    exercicio 12

--- 11_imc.py
+++ 11_imc.py
@@ -1,2 +1,8 @@
 '''Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que
 calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58'''
+
+def imc():
+    altura = float(input('Digite sua altura: '))
+    conta_imc = (72.7*altura) - 58
+    print(f'Seu imc é ${conta_imc}')
+
--- 12_imc_elaborado.py
+++ 12_imc_elaborado.py
@@ -3,3 +3,16 @@ que calcule seu peso ideal, utilizando as seguintes fórmulas:
 
     Para homens: (72.7*h) - 58
     Para mulheres: (62.1*h) - 44.7 '''
+
+def imc():
+    altura = float(input('Digite sua altura: '))
+    sexo = str(input('Digite f para feminino e m para masculino: '))
+    if sexo == "f":
+        conta_imc_f = (62.1 * altura) - 44.7
+        print(f'Seu imc é ${conta_imc_f}')
+    elif sexo == "m":
+        conta_imc_m = (72.7 * altura) - 58
+        print(f'Seu imc é ${conta_imc_m}')
+    else:
+        print(f'Informação sobre sexo incorreta, tente de novo')
+
