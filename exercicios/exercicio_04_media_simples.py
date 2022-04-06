863d29deb189f292594eb035449b8303478273fc
Jessica Arruda <sicarruda@gmail.com>
2022-04-06 15:39:59 -0300

    refatoração exericio 4 e criação do teste 04

--- exercicios/exercicio_04_media_simples.py
+++ exercicios/exercicio_04_media_simples.py
@@ -1,14 +1,16 @@
 '''Faça um Programa que peça as 4 notas bimestrais e mostre a média.'''
 
-print ('Olá, vamos saber qual a sua média deste ano? Primeiro me diga quais as suas 4 notas bimestrais')
-nota1 = int(input('Primeira nota: '))
-nota2 = int(input('Segunda nota: '))
-nota3 = int(input('Terceira nota: '))
-nota4 = int(input('Quarta nota: '))
 
 def media_notas(nota1, nota2, nota3, nota4):
     media = (nota1 + nota2 + nota3 + nota4) / 4
-    print ('Sua média é ', media)
+    return f'Sua média é {media}'
 
 
-media_notas(nota1, nota2, nota3, nota4)
+if __name__ == "__main__":
+    print ('Olá, vamos saber qual a sua média deste ano? Primeiro me diga quais as suas 4 notas bimestrais')
+    nota1 = int(input('Primeira nota: '))
+    nota2 = int(input('Segunda nota: '))
+    nota3 = int(input('Terceira nota: '))
+    nota4 = int(input('Quarta nota: '))
+
+    print(media_notas(nota1, nota2, nota3, nota4))
---
+++ tests/exercicio_04_media_simples_test.py
@@ -0,0 +1,9 @@
+from exercicios.exercicio_04_media_simples import media_notas
+
+def test_verifica_media_de_notas():
+    n1 = 1
+    n2 = 1
+    n3 = 1
+    n4 = 1
+    media = (n1 + n2 + n3 + n4) / 4
+    assert media_notas(n1,n2,n3,n4) == f'Sua média é {media}'
