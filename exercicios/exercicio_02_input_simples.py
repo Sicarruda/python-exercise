ac8f2f8810bbd991d1821d4b353cbcd5aaac05e1
Jessica Arruda <sicarruda@gmail.com>
2022-04-06 22:12:43 -0300

    refatoração do exercicio 2 e testes

--- exercicios/exercicio_02_input_simples.py
+++ exercicios/exercicio_02_input_simples.py
@@ -1,9 +1,12 @@
 '''Faça um Programa que peça um número e então mostre a mensagem O número
 informado foi [número].'''
 
-def print_mensagem_usuario(valor):
+def formata_mensagem_usuario(valor):
+    if type(valor) != int:
+        raise TypeError('Valor não permitido')
     return ( f'O numero informado foi {valor}')
     
 
 if __name__ == '__main__':
     numero = int(input('Digite um numero: '))
+    print(formata_mensagem_usuario(numero))
