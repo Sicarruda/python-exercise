38cbc6428388c2325499ace57497b6a06f434bee
Jessica Arruda <sicarruda@gmail.com>
2024-04-29 16:39:45 -0300

    Organiza e elimina codigo repedido

--- PYTHON_CRASH_COURSE/algoritmos/insertion_sort.py
+++ PYTHON_CRASH_COURSE/algoritmos/insertion_sort.py
@@ -1,23 +1,6 @@
-import random
-from datetime import datetime
+from time_comparer import *
 
 insertion_sort_test_list = [10, 100, 1000, 10000]
-# dd/mm/YY H:M:S:f
-format_date = "%d/%m/%Y %H:%M:%S:%f"
-
-# cria o array
-def create_unordered_array(array_len):
-    unordered_array = []
-    created_array = True
-
-    while created_array:
-        item = random.randrange(1, array_len)
-        unordered_array.append(item)
-
-        if len(unordered_array) == array_len:
-            created_array = False
-
-    return unordered_array
 
 
 # ordena o array
@@ -44,28 +27,6 @@ def insertion_sort(array):
         control += 1
     return array
 
-# exibe a diferença entre o tempo de inicio e o fim da execução 
-def time_diff(start, finish, array):
-    diff = finish - start
-    msg = f'Array com {array} elementos. Tempo de execução {diff}'
+compare_ordering_time(insertion_sort_test_list, insertion_sort)
 
-    return msg
-
-def compare_ordering_time(list_comparation):
-    for item in list_comparation:
-        list_unordered = create_unordered_array(item)
-
-        # Objeto datetime contem o a data e a hora atual
-        time_start = datetime.now()
-        format_string = time_start.strftime(format_date)
-        print("Data inicio = ", format_string)
-
-        insertion_sort(list_unordered)
-
-        time_finish = datetime.now()
-        format_string_2 = time_finish.strftime(format_date)
-        print("Data fim = ",format_string_2)
-
-        print(time_diff(time_start, time_finish, item))
-
-compare_ordering_time(insertion_sort_test_list)
+# TODO fazer insertion_sort com list compreension
---
+++ PYTHON_CRASH_COURSE/algoritmos/time_comparer.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from unordered_arrays import *
+from testes import *
+
+
+# dd/mm/YY H:M:S:f
+format_date = "%d/%m/%Y %H:%M:%S:%f"
+
+# exibe a diferença entre o tempo de inicio e o fim da execução 
+def time_diff(start, finish, array):
+    diff = finish - start
+    msg = f'Array com {array} elementos. Tempo de execução {diff}'
+
+    return msg
+
+# executa o algoritmo e devolve o tempo de execução e o teste
+def compare_ordering_time(list_comparation, algorithm):
+    for item in list_comparation:
+        list_unordered = create_unordered_array(item)
+
+        # Objeto datetime contem o a data e a hora atual
+        time_start = datetime.now()
+        format_string = time_start.strftime(format_date)
+        print("Data inicio = ", format_string)
+
+        array_ordered = algorithm(list_unordered)
+
+        time_finish = datetime.now()
+        format_string_2 = time_finish.strftime(format_date)
+        print("Data fim = ",format_string_2)
+
+        print(time_diff(time_start, time_finish, item))
+        
+        test_ordenation(array_ordered)
