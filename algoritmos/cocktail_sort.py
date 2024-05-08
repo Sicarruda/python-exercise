c95729105f6e83c583a059a9fc4a0669517c87c0
Jessica Arruda <sicarruda@gmail.com>
2024-05-08 18:11:35 -0300

    implementação de cocktail sort

--- algoritmos/cocktail_sort.py
+++ algoritmos/cocktail_sort.py
@@ -1,6 +1,7 @@
 from time_comparer import *
 
-cocktail_sort_test_list = [10, 100, 1000]
+cocktail_sort_test_list = [10, 100, 1000,10000]
+
 
 def cocktail_sort(array):
     array_length = len(array)
@@ -20,4 +21,19 @@ def cocktail_sort(array):
                 array_length -= 1
                 continue
 
+        for i in range(len(array) - 1, array_length, -1):
+
+            if (i - 1) > 0:
+
+                if array[i] < array[i - 1]:
+                    bigger = array[i]
+                    array[i] = array[i - 1]
+                    array[i - 1] = bigger
+
+            else:
+                array_length -= 1
+                continue
+
     return array
+
+compare_ordering_time(cocktail_sort_test_list,cocktail_sort)
