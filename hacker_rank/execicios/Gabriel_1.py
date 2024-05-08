b354e5e61054889e7a0a520272baf4fd014e1d22
Jessica Arruda <sicarruda@gmail.com>
2024-05-08 19:18:36 -0300

    inicia palindromo_sem_str

--- hacker_rank/execicios/Gabriel_1.py
+++ hacker_rank/execicios/Gabriel_1.py
@@ -6,19 +6,20 @@ num1 = 12321
 num2 = 12341
 num3 = 136547898745631
 
+
 def palindromo_str(num):
     str_num = str(num)
     reverse_str_num = str_num[::-1]
     if reverse_str_num == str_num:
         return True
-    
+
     return False
 
 
 def palindromo_str_interavel(num):
     str_num = str(num)
     array_str_num = list(str_num)
-    len_array = len(array_str_num)-1
+    len_array = len(array_str_num) - 1
     palindromo = True
 
     for i in range(len_array):
@@ -27,13 +28,20 @@ def palindromo_str_interavel(num):
             palindromo = False
             break
 
-    return palindromo    
+    return palindromo
+
+
+# https://pt.stackoverflow.com/questions/559104/verificar-se-o-n%C3%BAmero-%C3%A9-pal%C3%ADndromo-em-c-sem-usar-vetor
 
 
 def palindromo_sem_str(num):
+    numero = num % 10
+    palindromo = 0
+    palindromo = palindromo * 10 + numero
+    
     pass
 
 
 print(palindromo_str_interavel(num1))
 print(palindromo_str_interavel(num2))
-print(palindromo_str_interavel(num3))   
+print(palindromo_str_interavel(num3))
