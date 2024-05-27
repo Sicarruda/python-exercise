05d2b2d9ddcdb6959c3c2e064aa238711632653c
Jessica Arruda <sicarruda@gmail.com>
2024-05-27 19:18:07 -0300

    ajusta minimumBribes_novo

--- exercicios_logica/execicios/new_year_chaos.py
+++ exercicios_logica/execicios/new_year_chaos.py
@@ -4,26 +4,48 @@
 #
 # The function accepts INTEGER_ARRAY q as parameter.
 
-def minimumBribes(q):
-    bribes_sum = 0
-    bribe = 0
-    msg = ""
+# def minimumBribes_meu(q):
+#     bribes_sum = 0
+#     msg = ""
     
-    for element in range(len(q)-1):
-        bribe = 0
-        if q[element] > q[element+1]:
-            bribe = q[element] - q[element+1]
-            if bribe > 2:
-                msg = "Too chaotic"
-                break
-                
-            else:
-                bribes_sum = bribes_sum + bribe
-    if msg:
-        print(msg)
-        
-    else:
-        print(bribes_sum)
+#     for element in range(len(q)-1):
+#         bribe = 0
 
-minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])
+#         if q[element] > q[element+1]:
+#             bribe = q[element] - q[element+1]
+
+#             if bribe > 2:
+#                 msg = "Too chaotic"
+#                 break
+#             else:
+#                 bribes_sum = bribes_sum + bribe
+
+#     if msg:
+#         print(msg)
+#     else:
+#         print(bribes_sum)
+
+
+def minimumBribes_novo(q):
+    bribes_sum = 0
+    max_bribes = 2 
+    
+    for index in range(len(q)):
+        
+        if q[index] - (index+1) > max_bribes:
+            print("Too chaotic")
+            return
+        
+        start = max(0, q[index] - max_bribes)
+
+        for place in range(start, index, 1):
+
+            if q[place] > q[index]:
+                bribes_sum+=1
+
+    print(bribes_sum)
+    return 
+
+
+minimumBribes_novo([1, 2, 5, 3, 7, 8, 6, 4])
 
